4b16e17a49aa5e6ced4cc6f196fda1f95e90e1bf
FJ-AKANDE <akandefortunatus2021@gmail.com>
2025-02-26 02:48:48 +0100

    feat: implement view count tracking; add write client for updating views and configure environment token

--- components/view.tsx
+++ components/view.tsx
@@ -2,6 +2,7 @@ import { client } from "@/sanity/lib/client";
 import Ping from "./ping";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { formatNumber } from "@/lib/utils";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
   const { views } = await client
@@ -9,6 +10,13 @@ const View = async ({ id }: { id: string }) => {
       useCdn: false,
     })
     .fetch(STARTUP_BY_ID_QUERY, { id });
+
+  writeClient
+    .patch(id)
+    .setIfMissing({ views: 0 })
+    .inc({ views: 1 })
+    .commit()
+    .catch((err) => console.error("Failed to update views:", err));
 
   return (
     <div className="fixed bottom-3 right-3 mt-5 flex items-center justify-end">
